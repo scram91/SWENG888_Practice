d936f0de6707bf93418b6bdd09de2006f1474727
Sam Cramer <samuel.c.cramer@gmail.com>
2024-03-18 17:32:07 -0400

    Add new practice 4 application

---
+++ PracticeIV/app/src/main/java/edu/psu/sweng888/practiceiv/MainActivity.java
@@ -0,0 +1,16 @@
+package edu.psu.sweng888.practiceiv;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    //TODO: THIS WILL BE THE SPLASH SCREEN
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
