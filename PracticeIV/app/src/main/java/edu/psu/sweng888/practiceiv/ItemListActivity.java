756f709f22ead357ac6c09f2bc15bb18ea8a4c4f
Sam Cramer <samuel.c.cramer@gmail.com>
2024-03-23 10:38:54 -0400

    finish lesson will fix errors later

---
+++ PracticeIV/app/src/main/java/edu/psu/sweng888/practiceiv/ItemListActivity.java
@@ -0,0 +1,4 @@
+package edu.psu.sweng888.practiceiv;
+
+public class ItemListActivity {
+}
--- PracticeIV/app/src/main/java/edu/psu/sweng888/practiceiv/MainActivity.java
+++ PracticeIV/app/src/main/java/edu/psu/sweng888/practiceiv/MainActivity.java
@@ -1,8 +1,11 @@
 package edu.psu.sweng888.practiceiv;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +14,28 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // on below line we are configuring our window to full screen
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // on below line we are
+                // creating a new intent
+                Intent i = new Intent(MainActivity.this, LoginActivity.class);
+
+                // on below line we are
+                // starting a new activity.
+                startActivity(i);
+
+                // on the below line we are finishing
+                // our current activity.
+                finish();
+            }
+        }, 2000);
     }
 }
